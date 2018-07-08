ffcde783c706cdc69c5aa9843e38567f0b95fc7f
dongphuong2102 <dongphuong2102@gmail.com>
2018-07-08 09:35:38 +0700

    Finish static pages

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,14 @@
-ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
-require 'rails/test_help'
+# frozen_string_literal: true
 
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+ENV["RAILS_ENV"] ||= "test"
+require_relative "../config/environment"
+require "rails/test_help"
 
+class ActiveSupport
+  # Setup all fixtures in test/fixtures/*.yml for all tests in
+  # alphabetical order.
+  class TestCase
+    fixtures :all
+  end
   # Add more helper methods to be used by all tests here...
 end
