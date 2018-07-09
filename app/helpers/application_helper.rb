e9a29f171c77b5cb8b7768dd49fffca415305991
dongphuong2102 <dongphuong2102@gmail.com>
2018-07-09 11:25:15 +0700

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  # Returns the full title on a per-page basis.       # Documentation comment
+  # Method def, optional arg
+  def full_title page_title = ""
+    base_title = t ".base_title"  # Variable assignment
+    if page_title.empty?                              # Boolean test
+      base_title                                      # Implicit return
+    else
+      page_title + " | " + base_title                 # String concatenation
+    end
+  end
 end
