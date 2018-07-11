13f8cb7e33db6d90436b2d0dd291dd805f4a5d5b
dongphuong2102 <dongphuong2102@gmail.com>
2018-07-11 10:28:00 +0700

    Implement basic login

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ git_source(:github){|repo| "https://github.com/#{repo}.git"}
 ruby "2.5.1"
 
 gem "bcrypt"
+gem "config"
 gem "rubocop", "~> 0.53.0"
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem "bootstrap-sass"
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by email: params[:session][:email].downcase
+    if user&.authenticate(params[:session][:password])
+      log_in user
+      check_remember user
+      redirect_to user
+    else
+      flash.now[:danger] = t(".invalid")
+      render :new
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
+  end
+end
