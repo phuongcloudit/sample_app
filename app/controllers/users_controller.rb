741c012a88797dfe73f534123800f98af6eb9be8
dongphuong2102 <dongphuong2102@gmail.com>
2018-07-16 14:20:35 +0700

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,6 +50,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def following
+    @title = t(".following")
+    load_user
+    @users = @user.following.paginate(page: params[:page])
+    render "show_follow"
+  end
+
+  def followers
+    @title = t(".followers")
+    load_user
+    @users = @user.followers.paginate(page: params[:page])
+    render "show_follow"
+  end
+
   private
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,13 @@ Rails.application.routes.draw do
   resources :account_activations, only: %i(edit)
   resources :password_resets, only: %i(new create edit update)
   resources :microposts, only: %i(create destroy)
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :account_activations, only: %i(edit)
+  resources :password_resets, only: %i(new create edit update)
+  resources :microposts, only: %i(create destroy)
+  resources :relationships, only: %i(create destroy)
 end
